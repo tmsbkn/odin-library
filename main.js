64562ba98fe17498e75bf1ab6d4af50b270ff5b3
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-05 19:40:06 -0400

    Add function to add book to library

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
+const title = document.getElementById('bookNameInput');
+const author = document.getElementById('bookAuthorInput');
+const pages = document.getElementById('bookPagesInput');
+const read = document.getElementById('bookReadInput');
+
 const myLibrary = [];
 
+const addBookBtn = document.getElementById('addBookBtn');
+addBookBtn.addEventListener('click', () => {
+   if (addBookForm.style.display == 'flex') {
+      addBookForm.style.display = 'none';
+   } else {
+      addBookForm.style.display = 'flex';
+   }
+});
+
+const submit = document.querySelector('form');
+const bookShelf = document.getElementById('app');
 function Book(name, author, pages, read) {
-   this.name = name;
-   this.author = author;
-   this.pages = pages;
-   this.read = read;
+   this.bookID = self.crypto.randomUUID();
+   this.name = name.value;
+   this.author = author.value;
+   this.pages = pages.value;
+   this.read = read.value;
    this.info = function () {
-      console.log(name + ' by ' + author + ', ' + pages + ' pages, ' + read);
+      console.log(
+         name + ' by ' + author + ', ' + pages + ' pages, ' + read + bookID,
+      );
    };
 }
 
-function addBookToLibrary(name, author, pages, read) {
-   book = new Book(name, author, pages, read);
-   myLibrary.push(book);
+function addBookToLibrary() {
+   newBook = new Book(title, author, pages, read);
+   myLibrary.push(newBook);
    console.log(myLibrary);
 }
+
+submit.addEventListener('submit', (e) => {
+   e.preventDefault();
+   addBookToLibrary();
+});
