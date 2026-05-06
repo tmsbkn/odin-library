e308526dc05f2ab275cbcab23a648e7aa52e5b50
tmsbkn <54986052+tmsbkn@users.noreply.github.com>
2026-05-05 22:59:44 -0400

    Add status toggles

--- main.js
+++ main.js
@@ -35,19 +35,28 @@ function buildBookshelf() {
    bookShelf.innerHTML = '';
    myLibrary.forEach((book) => {
       const bookNode = document.createElement('div');
-      bookNode.innerHTML = `
+      if (book.read === 'read') {
+         bookNode.innerHTML = `
       <span class="book-title">${book.name}</span>
       <cite class="book-author">${book.author}</cite>
       <span class="book-pages">${book.pages} Pages</span>
-      <div id="${book.bookID}" class="delete">X</div>
+      <span data-bookIndex="${book.bookID}" class="book-status read">Read</span>
+      <div data-bookIndex="${book.bookID}" class="delete">X</div>
       `;
-      if (book.read === 'read') {
-         bookNode.classList.add('read');
+      } else {
+         bookNode.innerHTML = `
+      <span class="book-title">${book.name}</span>
+      <cite class="book-author">${book.author}</cite>
+      <span class="book-pages">${book.pages} Pages</span>
+      <span data-bookIndex="${book.bookID}" class="book-status">Unread</span>
+      <div data-bookIndex="${book.bookID}" class="delete">X</div>
+      `;
       }
       bookNode.classList.add('book');
       bookShelf.append(bookNode);
    });
    buildDeleteButtons();
+   buildReadButtons();
 }
 
 function addBookToLibrary() {
@@ -81,7 +90,28 @@ function buildDeleteButtons() {
    var deleteButton = document.querySelectorAll('.delete');
    deleteButton.forEach((button) => {
       button.addEventListener('click', (e) => {
-         deleteBook(e.id);
+         deleteBook(e.target.getAttribute('data-bookIndex'));
+      });
+   });
+}
+
+function changeStatus(id) {
+   let bookToUpdate = myLibrary.findIndex((obj) => {
+      return (obj.bookID = id);
+   });
+   if (myLibrary[bookToUpdate].read === 'read') {
+      myLibrary[bookToUpdate].read = 'unread';
+   } else {
+      myLibrary[bookToUpdate].read = 'read';
+   }
+   buildBookshelf();
+}
+
+function buildReadButtons() {
+   var readButton = document.querySelectorAll('.book-status');
+   readButton.forEach((button) => {
+      button.addEventListener('click', (e) => {
+         changeStatus(e.target.getAttribute('data-bookIndex'));
       });
    });
 }
